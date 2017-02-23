d662852d109274e95942bc30ef49413cf387aabc
Brandon Malone <bmmalone@gmail.com>
2017-02-24 00:50:16 +0100

    UPD basic train-auto-sklearn pipeline

--- as_auto_sklearn/train_auto_sklearn.py
+++ as_auto_sklearn/train_auto_sklearn.py
@@ -9,38 +9,33 @@ import sklearn.cross_validation
 import sklearn.pipeline
 
 from autosklearn.regression import AutoSklearnRegressor
+from aslib_scenario.aslib_scenario import ASlibScenario
 
 import misc.automl_utils as automl_utils
+import misc.math_utils as math_utils
 import misc.utils as utils
 
 import logging
 import misc.logging_utils as logging_utils
 logger = logging.getLogger(__name__)
 
-#from bn_portfolio.bn_portfolio_utils import ColumnSelector, fields_mapping, solvers, prediction_fields
-
-import bn_portfolio.bn_portfolio_utils as bn_portfolio_utils
-import bn_portfolio.bn_portfolio_filenames as filenames
-
-feature_set_choices = bn_portfolio_utils.fields_mapping.keys()
-
 def get_pipeline(args):
     pipeline_steps = []
 
-    # figure out what our feature set looks like
-    feature_set_fields = np.array(bn_portfolio_utils.fields_mapping[args.feature_set])
-
+    # TODO: create the feature filter
     # the first step in the pipeline is filtering the forbidden features
-    feature_set_selector = bn_portfolio_utils.ColumnSelector(feature_set_fields, 
-        transform_contiguous=True)
+    #feature_set_selector = bn_portfolio_utils.ColumnSelector(
+    #    feature_set_fields, 
+    #    transform_contiguous=True
+    #)
 
-    fs = ('feature_set', feature_set_selector)
-    pipeline_steps.append(fs)
+    #fs = ('feature_set', feature_set_selector)
+    #pipeline_steps.append(fs)
     
-    # optionally, we may standardize the data
-    if args.standarize:
-        s = ('scaler', sklearn.preprocessing.StandardScaler())
-        pipeline_steps.append(s)
+    # TODO: optionally, we may standardize the data
+    #if args.standarize:
+    #    s = ('scaler', sklearn.preprocessing.StandardScaler())
+    #    pipeline_steps.append(s)
 
     # then we use the auto-sklearn options
         
@@ -57,8 +52,6 @@ def get_pipeline(args):
     r = ('automl', regressor)
     pipeline_steps.append(r)
 
-        
-
     # and create the pipeline
     pipeline = sklearn.pipeline.Pipeline(pipeline_steps)
 
@@ -70,78 +63,62 @@ def main():
         "writes them to disk. Importantly, it *does not* collect any statistics, "
         "make predictions, etc.")
 
-    parser.add_argument('instances', help="The preprocessed data")
+    parser.add_argument('scenario', help="The ASlib scenario")
     
-    parser.add_argument('feature_set', help="The set of features to use for this "
-        "experiment", choices=feature_set_choices)
-
     parser.add_argument('solver', help="The solver for which models will be "
-        "learned", choices=bn_portfolio_utils.solvers)
+        "learned")
 
     parser.add_argument('fold', help="The fold for which a model wil be learned",
         type=int)
 
-    parser.add_argument('name', help="The base name for the output pickle files")
+    parser.add_argument('out', help="The output (pickle) file")
 
-    parser.add_argument('--standardize', help="If this flag is given, the data "
-        "will be standardized (zero mean, unit variance) before learning. N.B. "
-        "This script *does not* write the standardized scaler to disk. It must "
-        "be recomputed at test time.", action='store_true')
+    parser.add_argument('--config', help="A (yaml) config file which specifies "
+        "options controlling the learner behavior")
 
     automl_utils.add_automl_options(parser)
     logging_utils.add_logging_options(parser)
     args = parser.parse_args()
     logging_utils.update_logging(args)
 
-    msg = "Reading in the preprocessed data"
+    math_utils.check_range(args.fold, 1, 10, variable_name="fold")
+
+    msg = "Reading ASlib scenario"
     logger.info(msg)
-    instances = pd.read_csv(args.instances)
+    scenario = ASlibScenario()
+    scenario.read_scenario(args.scenario)
+
+    # ensure the selected solver is present
+    if args.solver not in scenario.algorithms:
+        msg = ("[train-auto-sklear]: the solver '{}' is not present in the "
+            "ASlib scenario".format(args.solver))
+        raise ValueError(msg)
+    
+    msg = "Solver: {}, Fold: {}".format(args.solver, args.fold)
+    logger.info(msg)
 
     msg = "Constructing pipeline"
     logger.info(msg)
     pipeline = get_pipeline(args)
 
-    msg = "Removing fields with null values"
+    msg = "Extracting solver and fold performance data"
     logger.info(msg)
+    testing, training = scenario.get_split(args.fold)
 
-    for f in bn_portfolio_utils.fields_mapping[args.feature_set]:
-        m_nan = instances[f].isnull()
-        if sum(m_nan) > 0:
-            instances = instances[~m_nan]
+    X_train = training.feature_data.values
+    y_train = training.performance_data[args.solver].values
 
-    msg = "Splitting data into 10-folds"
-    logger.info(msg)
-
-    cv = sklearn.cross_validation.KFold(len(instances), n_folds=10, shuffle=True, 
-        random_state=args.seed)
-
-    solver_runtimes = instances[args.solver]
-    prediction_field = "{}_prediction".format(args.solver)
-    error_field = "{}_error".format(args.solver)
-
-    instances[prediction_field] = 0
-    train, test = utils.nth(cv, args.fold)
-
-    msg = "Solver: {}, Fold: {}".format(args.solver, args.fold)
-    logger.info(msg)
-
-    # note, these create COPIES NOT VIEWS
-    X_train, X_test = instances.iloc[train], instances.iloc[test], 
-    y_train, y_test = solver_runtimes.iloc[train], solver_runtimes.iloc[test]
-    
-    # must be numpy for automl
-    y_train = y_train.values
+    X_train=np.ascontiguousarray(X_train)
+    y_train=np.ascontiguousarray(y_train)
 
     # fit the pipeline on X_train and y_train
     pl_fit = pipeline.fit(X_train, y_train)
 
     automl = pl_fit.named_steps['automl']
 
-    fn = filenames.aml_pkl(args.out, args.name, args.solver, args.feature_set, 
-        args.fold)
-    msg = "Writing ensemble to disk: {}".format(fn)
+    msg = "Writing auto-sklearn ensemble to disk: {}".format(args.out)
     logger.info(msg)
-    automl_utils.write_automl(automl, fn)
+    automl_utils.write_automl(automl, args.out)
 
 if __name__ == '__main__':
     main()
