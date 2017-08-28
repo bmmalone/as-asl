176a94dd4b0721a3e58f7499a24396718ee21f49
Brandon Malone <bmmalone@gmail.com>
2017-08-28 17:26:45 +0200

    ADD pipeline wrapper class for the ensemble

--- as_auto_sklearn/as_asl_ensemble.py
+++ as_auto_sklearn/as_asl_ensemble.py
@@ -2,9 +2,13 @@ import autosklearn.metrics
 
 import numpy as np
 import pandas as pd
+
+import sklearn.pipeline
 import sklearn.preprocessing
+from sklearn.utils.validation import check_is_fitted
 
 import misc.automl_utils as automl_utils
+from misc.nan_standard_scaler import NaNStandardScaler
 import misc.parallel as parallel
 
 class ASaslEnsemble:
@@ -22,6 +26,30 @@ class ASaslEnsemble:
         
         model_fit = model.fit(self.X_train, y_train, metric=metric)
         return (solver, model_fit)
+
+
+    def _fit_init(self, X_train, y_train):
+        
+        # make sure we can encode our algorithm labels
+        self.le = sklearn.preprocessing.LabelEncoder()
+        self.le_ = self.le.fit(self.solvers)
+        
+        # create the solver-specific datasets
+        self.y_train = {
+            solver: y_train[solver]
+                for solver in self.solvers
+        }
+        
+        # and save the training dataset
+        if isinstance(X_train, pd.DataFrame):
+            self.X_train = X_train.values
+        else:
+            self.X_train = X_train
+        
+        self.orig_y_train = y_train
+        
+        return self
+
     
     def _fit_regressors(self):
         
@@ -40,8 +68,10 @@ class ASaslEnsemble:
         )
 
         self.solver_asl_regressors_ = dict(ret)
+
+        return self
         
-    def _fit_stacking_model(self, metric):
+    def _fit_stacking_model(self, metric=autosklearn.metrics.f1_micro):
         
         # use those to build the dataset for the stacking model
         X_stacking_train = pd.DataFrame()
@@ -62,7 +92,7 @@ class ASaslEnsemble:
 
         # and fit
         self.stacking_model = automl_utils.AutoSklearnWrapper(
-            estimator_named_step="classifer", args=args)
+            estimator_named_step="classifer", args=self.args)
 
         self.stacking_model_ = self.stacking_model.fit(
             self.X_stacking_train,
@@ -71,25 +101,7 @@ class ASaslEnsemble:
         )
         
         return self
-    
-    def _fit_init(self, X_train, y_train):
-        
-        # make sure we can encode our algorithm labels
-        self.le = sklearn.preprocessing.LabelEncoder()
-        self.le_ = self.le.fit(self.solvers)
-        
-        # create the solver-specific datasets
-        self.y_train = {
-            solver: y_train[solver]
-                for solver in self.solvers
-        }
-        
-        # and save the training dataset
-        self.X_train = X_train.values
-        self.orig_y_train = y_train
-        
-        return self
-        
+            
     def fit(self, X_train, y_train, metric=autosklearn.metrics.f1_micro):
         
         self._fit_init(X_train, y_train)
@@ -139,3 +151,98 @@ class ASaslEnsemble:
         y_pred = y_pred.argmax(axis=1)
         
         return y_pred
+
+class ASaslPipeline:
+    def __init__(self, args):
+        self.args = args
+
+    def fit(self, scenario):
+        """ Fit the pipeline using the ASlibScenario
+        """
+            
+        nss = NaNStandardScaler()
+        as_asl_ensemble = ASaslEnsemble(
+            args=self.args,
+            solvers=scenario.algorithms
+        )
+        
+        pipeline = [
+            ('nss', nss),
+            ('selector', as_asl_ensemble)
+        ]
+        
+        self.pipeline = sklearn.pipeline.Pipeline(pipeline)
+
+        self.X_train = scenario.feature_data.values
+        self.y_train = scenario.performance_data
+
+        self.pipeline_ = self.pipeline.fit(self.X_train, self.y_train)
+
+        return self
+
+    def predict_proba(self, X_test):
+        """ Use the fit pipeline to predict the likelihood that each solver is
+        optimal for each instance
+        """
+
+        check_is_fitted(self, "pipeline_")
+        y_proba_pred = self.pipeline_.predict_proba(X_test)
+        return y_proba_pred
+
+    def predict(self, X_test):
+        """ Use the fit pipeline to select the solver most likely to be optimal
+        for each instance
+        """
+
+        check_is_fitted(self, "pipeline_")
+        y_proba_pred = self.predict_proba(X_test)
+        y_pred = y_proba_pred.argmax(axis=1)
+        return y_pred
+
+    def transform(self, y, *_):
+        """ Use the label encodings in the pipeline to transform the labels
+        """
+        check_is_fitted(self, "pipeline_")
+        as_asl_ensemble = self.pipeline_.named_steps['selector']
+        y_transformed = as_asl_ensemble.le_.transform(y)
+        return y_transformed
+
+    def inverse_transform(self, y, *_):
+        """ Use the label encodings in the pipeline to transform the indices
+        """
+        check_is_fitted(self, "pipeline_")
+        as_asl_ensemble = self.pipeline_.named_steps['selector']
+        y_transformed = as_asl_ensemble.le_.inverse_transform(y)
+        return y_transformed
+
+            
+    def create_solver_schedules(self, scenario):
+        """ Use the fit pipeline to create solver schedules for the scenario
+        instances
+        """
+
+        # currently, just take the predicted best solver
+        X_test = scenario.feature_data
+        y_test = scenario.performance_data
+
+        X_test = X_test.values
+        y_pred = self.predict(X_test)
+
+        choices = self.inverse_transform(y_pred)
+
+        it = zip(choices, scenario.instances)
+        predictions = {}
+        
+        for choice, instance in it:
+            if scenario.performance_type[0] == "runtime":
+                predictions[instance] = [[choice,scenario.algorithm_cutoff_time]]
+            elif testing.performance_type[0] == "solution_quality":
+                predictions[instance] = [(choice,999999999999)]
+
+        return predictions
+
+
+
+
+
+
